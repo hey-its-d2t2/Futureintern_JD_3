e07592dcae3b455eb778d2d3e89ed58f3e924475
Deepak Singh <deepsinghkumar01@gmail.com>
2024-09-06 12:48:59 +0530

    Game Service class implemented.

--- src/main/java/com/Futureintern_JD_3/Service/GameService.java
+++ src/main/java/com/Futureintern_JD_3/Service/GameService.java
@@ -1,4 +1,19 @@
 package com.Futureintern_JD_3.Service;
 
+import com.Futureintern_JD_3.Model.Game;
+
 public class GameService {
+    private Game game = new Game();
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void makeMove(int row, int col) {
+        game.makeMove(row, col);
+    }
+
+    public void resetGame() {
+        game.resetGame();
+    }
 }
