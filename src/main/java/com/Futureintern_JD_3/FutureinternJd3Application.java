23b5cfaf9311838416a89f5f41c43eb6090a9a34
Deepak Singh <deepsinghkumar01@gmail.com>
2024-09-06 11:46:42 +0530

    Initial Commit: Project created form start.sprin.io, with dependencies speing boot web, thymeleaf,Mavaen, jar, java 21

---
+++ src/main/java/com/Futureintern_JD_3/FutureinternJd3Application.java
@@ -0,0 +1,13 @@
+package com.Futureintern_JD_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureinternJd3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureinternJd3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Futureintern_JD_3/FutureinternJd3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Futureintern_JD_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutureinternJd3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
